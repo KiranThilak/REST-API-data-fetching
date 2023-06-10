44597c7a685ccc448883b879d47db17c9e53e80d
kiranthilak <kiran.thilak@yahoo.com>
2023-06-10 09:16:57 +0530

    useeffect added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
 function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/";
+  const [reqType, setReqType] = useState("users");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchItems();
+  }, [reqType]);
   return <div className="App"></div>;
 }
 
